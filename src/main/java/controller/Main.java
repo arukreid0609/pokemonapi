4af77bb14bcf20719f479cf3033b0ab112244b59
sasakiy916 <sasaki3y98521j@gmail.com>
2023-09-13 02:22:09 +0900

    first commit

---
+++ src/main/java/controller/Main.java
@@ -0,0 +1,39 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Pokemon;
+import model.PokemonParser;
+
+@WebServlet("/Main")
+public class Main extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// フォワード
+		RequestDispatcher rd = request.getRequestDispatcher("");
+		rd.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// リクエストパラメータの取得
+		request.setCharacterEncoding("UTF-8");
+		int id = Integer.parseInt(request.getParameter("id"));
+
+		// 図鑑番号からポケモンのデータ取得
+		PokemonParser parser = new PokemonParser();
+		Pokemon pokemon = parser.getPokemon(id);
+
+		// リクエストスコープに保存
+		request.setAttribute("pokemon", pokemon);
+		// フォワード
+		RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/view/result.jsp");
+		rd.forward(request, response);
+	}
+}
---
+++ src/main/java/model/PokemonParser.java
@@ -0,0 +1,115 @@
+package model;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.stream.JsonReader;
+
+public class PokemonParser {
+
+	// ポケモンのデータ１匹分取得
+	public Pokemon getPokemon(int id) {
+		String urlString = "https://pokeapi.co/api/v2/pokemon/" + id;
+		Pokemon pokemon = null;
+
+		JsonObject root = this.getRoot(urlString);
+		if (root != null) {
+			pokemon = new Pokemon();
+			// 高さ、体重の取得
+			double height = root.get("height").getAsDouble() / 10;
+			double weight = root.get("weight").getAsDouble() / 10;
+			pokemon.setHeight(height);
+			pokemon.setWeight(weight);
+			// 画像の取得
+			JsonObject sprites = root.get("sprites").getAsJsonObject();
+			JsonObject other = sprites.get("other").getAsJsonObject();
+			JsonObject officialArtWork = other.get("official-artwork").getAsJsonObject();
+			String sprite = officialArtWork.get("front_default").getAsString();
+			pokemon.setSprite(sprite);
+			
+			// 名前とテキストの取得
+			this.setSpecies(pokemon, id);
+		}
+
+		return pokemon;
+	}
+
+	// 名前とフレーバーテキストを取得、設定
+	private void setSpecies(Pokemon pokemon, int id) {
+		String urlString = "https://pokeapi.co/api/v2/pokemon-species/" + id;
+		JsonObject root = getRoot(urlString);
+
+		// 名前取得
+		JsonArray names = root.get("names").getAsJsonArray();
+		for(JsonElement element : names) {
+			JsonObject object = element.getAsJsonObject();
+			JsonObject language = object.get("language").getAsJsonObject();
+			String languageName = language.get("name").getAsString();
+			// 最初に見つけた日本語名を取得
+			if (languageName.equals("ja")) {
+				String name = object.get("name").getAsString();
+				pokemon.setName(name);
+				break;
+			}
+		}
+
+		// フレーバーテキスト取得
+		JsonArray flavorTextEntries = root.get("flavor_text_entries").getAsJsonArray();
+		for (JsonElement element : flavorTextEntries) {
+			JsonObject object = element.getAsJsonObject();
+			JsonObject language = object.get("language").getAsJsonObject();
+			String languageName = language.get("name").getAsString();
+			// 最初に見つけた日本語テキストを取得
+			if (languageName.equals("ja")) {
+				String text = object.get("flavor_text").getAsString();
+				text = text.replace("\n", "<br>");// 改行コードをhtmlの<br>タグに変更
+				pokemon.setText(text);
+				break;
+			}
+		}
+	}
+
+	// ルートオブジェクトを取得
+	private JsonObject getRoot(String urlString) {
+		JsonReader reader = null;
+		HttpURLConnection con = null;
+		JsonObject root = null;
+		try {
+			// URLを指定、接続、GETリクエストを設定
+			URL url = new URL(urlString);
+			con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+			// GETリクエストして返ってきたレスポンス内容読み込む
+			InputStream is = con.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+			// レスポンス内容をJsonとして読み込む
+			reader = new JsonReader(isr);
+			// Jsonからroot要素を取得（今回は「{ }」から始まるオブジェクト）
+			root = new Gson().fromJson(reader, JsonObject.class);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if (con != null) {
+				con.disconnect();
+			}
+		}
+		return root;
+	}
+}
